482c963ccaad65029e6c0f3caecd4ae518b3213e
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2022-02-24 14:14:36 +0100

    Add linters clang-format, clang-tidy, cmake-format and cmake-lint

---
+++ cmake/linter-tools.cmake
@@ -0,0 +1,38 @@
+# Additional targets for linters
+# Uses clang-format, clang-tidy and cmakelang (cmake-format and cmake-lint)
+
+# Ref: https://stackoverflow.com/questions/32280717/cmake-clang-tidy-or-other-script-as-custom-target
+
+file(GLOB_RECURSE ALL_SOURCE_FILES *.cpp *.hpp)
+file(GLOB_RECURSE ALL_CMAKE_LISTS CMakeLists.txt)
+
+add_custom_target(
+    clang-format
+    COMMAND clang-format
+    -style=file
+    -i
+    ${ALL_SOURCE_FILES}
+)
+
+add_custom_target(
+    clang-tidy
+    COMMAND clang-tidy
+    ${ALL_SOURCE_FILES}
+    -config=''
+    --
+    -std=c++11
+    ${INCLUDE_DIRECTORIES}
+)
+
+add_custom_target(
+    cmake-format
+    COMMAND cmake-format
+    -i
+    ${ALL_CMAKE_LISTS}
+)
+
+add_custom_target(
+    cmake-lint
+    COMMAND cmake-lint
+    ${ALL_CMAKE_LISTS}
+)
