73b3a33b20d831353c8eea63f355094fd7cf1fa5
Faruk D <f.diblen@esciencecenter.nl>
2022-03-03 14:57:38 +0100

    add exhale and breathe extensions to sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,8 @@ release = '0.3.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'breathe',
+    'exhale'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
