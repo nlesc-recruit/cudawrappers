d082aa6f67be2694f933ab97c0aab4064f006e75
Bouwe Andela <b.andela@esciencecenter.nl>
2022-04-22 18:02:06 +0200

    Fix some linter errors

--- src/cufft.cpp
+++ src/cufft.cpp
@@ -1,47 +1,30 @@
 #include "cufft.hpp"
 
+#include <array>
+
 namespace cufft {
 
-const char *Error::what() const noexcept {
-  switch (_result) {
-    case CUFFT_SUCCESS:
-      return "success";
-    case CUFFT_INVALID_PLAN:
-      return "invalid plan";
-    case CUFFT_ALLOC_FAILED:
-      return "alloc failed";
-    case CUFFT_INVALID_TYPE:
-      return "invalid type";
-    case CUFFT_INVALID_VALUE:
-      return "invalid value";
-    case CUFFT_INTERNAL_ERROR:
-      return "internal error";
-    case CUFFT_EXEC_FAILED:
-      return "exec failed";
-    case CUFFT_SETUP_FAILED:
-      return "setup failed";
-    case CUFFT_INVALID_SIZE:
-      return "invalid size";
-    case CUFFT_UNALIGNED_DATA:
-      return "unaligned data";
-    case CUFFT_INCOMPLETE_PARAMETER_LIST:
-      return "incomplete parameter list";
-    case CUFFT_INVALID_DEVICE:
-      return "invalid device";
-    case CUFFT_PARSE_ERROR:
-      return "parse error";
-    case CUFFT_NO_WORKSPACE:
-      return "no workspace";
-    case CUFFT_NOT_IMPLEMENTED:
-      return "not implemented";
-    case CUFFT_LICENSE_ERROR:
-      return "license error";
-    case CUFFT_NOT_SUPPORTED:
-      return "not supported";
+const std::array<const char *const, MAX_CUFFT_ERROR> CUFFT_STATUS_MSG = {
+    "success",
+    "invalid plan",
+    "alloc failed",
+    "invalid type",
+    "invalid value",
+    "internal error",
+    "exec failed",
+    "setup failed",
+    "invalid size",
+    "unaligned data",
+    "incomplete parameter list",
+    "invalid device",
+    "parse error",
+    "no workspace",
+    "not implemented",
+    "license error",
+    "not supported"};
 
-    default:
-      return "unknown";
-  }
+const char *Error::what() const noexcept {
+  return CUFFT_STATUS_MSG.at(_result);
 }
 
 }  // namespace cufft
