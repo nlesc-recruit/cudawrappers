c5cddc30bc371579cddd3000376ef947b3c4a499
Bouwe Andela <b.andela@esciencecenter.nl>
2022-07-28 15:28:44 +0200

    Add some tests for nvrtc

---
+++ tests/test_nvrtc.cpp
@@ -0,0 +1,28 @@
+#include <catch2/catch.hpp>
+#include <string>
+#include <vector>
+
+#include "nvrtc.hpp"
+
+TEST_CASE("Test nvrtc::Program", "[nvrtc::Program]") {
+  const std::string kernel = R"(
+    __global__ void vector_add(float *c, float *a, float *b, int n) {
+      int i = blockIdx.x * blockDim.x + threadIdx.x;
+      if (i < n) {
+        c[i] = a[i] + b[i];
+      }
+    }
+  )";
+
+  nvrtc::Program program(kernel, "kernel.cu");
+
+  SECTION("Test Program.compile") {
+    const std::vector<std::string> options = {"--generate-line-info"};
+    REQUIRE_NOTHROW(program.compile(options));
+  }
+
+  SECTION("Test Program.getPTX") {
+    const std::string ptx{program.getPTX()};
+    REQUIRE(ptx.size() > 0);
+  }
+}
