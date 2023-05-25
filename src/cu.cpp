77d72fc3c5e72f25e05190547ed6199f4ff5ac2a
Leon Oostrum <l.oostrum@esciencecenter.nl>
2023-05-25 15:26:58 +0200

    Remove Source class, superseded by NVRTC

--- src/cu.cpp
+++ src/cu.cpp
@@ -19,19 +19,6 @@ Context Device::primaryCtxRetain() {
   return {context, *this};
 }
 
-void Source::compile(const char *output_file_name,
-                     const char *compiler_options) {
-  std::stringstream command_line;
-  command_line << "nvcc -cubin " << compiler_options << " -o "
-               << output_file_name << ' ' << _input_file_name;
-  // #pragma omp critical (clog)
-  //  std::clog << command_line.str() << std::endl;
-
-  int retval = system(command_line.str().c_str());
-
-  if (WEXITSTATUS(retval) != 0) throw Error(CUDA_ERROR_INVALID_SOURCE);
-}
-
 void DeviceMemory::zero(size_t size) {
   checkCudaCall(cuMemsetD8(_obj, 0, size));
 }
