849f5575d9a25009d253b27c25044acb8f70a55d
Leon Oostrum <l.oostrum@esciencecenter.nl>
2023-06-23 16:33:27 +0200

    Bump version: 0.3.0 → 0.4.0

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, cudawrappers developers'
 author = 'cudawrappers developers'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.0'
+release = '0.4.0'
 
 
 # -- General configuration ---------------------------------------------------
