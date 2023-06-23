45521b0a5980940f64a7f6264e93b77dbe048cf5
Bram Veenboer <bram.veenboer@gmail.com>
2023-06-23 14:07:20 +0200

    Add test for cu::DeviceMemory::zero

--- tests/test_cu.cpp
+++ tests/test_cu.cpp
@@ -63,3 +63,58 @@ TEST_CASE("Test copying cu::DeviceMemory and cu::HostMemory using cu::Stream",
     CHECK(!static_cast<bool>(memcmp(src, tgt, size)));
   }
 }
+
+TEST_CASE("Test zeroing cu::DeviceMemory", "[zero]") {
+  cu::init();
+  cu::Device device(0);
+  cu::Context context(CU_CTX_SCHED_BLOCKING_SYNC, device);
+
+  SECTION("Test zeroing cu::DeviceMemory asynchronously") {
+    const size_t N = 3;
+    const size_t size = N * sizeof(int);
+    cu::HostMemory src(size);
+    cu::HostMemory tgt(size);
+
+    // Populate the memory with values
+    int* const src_ptr = static_cast<int*>(src);
+    int* const tgt_ptr = static_cast<int*>(tgt);
+    for (int i = 0; i < N; i++) {
+      src_ptr[i] = i;
+      tgt_ptr[i] = 0;
+    }
+    cu::DeviceMemory mem(size);
+
+    cu::Stream stream;
+    stream.memcpyHtoDAsync(mem, src, size);
+    mem.zero(size, stream);
+    stream.memcpyDtoHAsync(tgt, mem, size);
+    stream.synchronize();
+
+    CHECK(static_cast<bool>(memcmp(src, tgt, size)));
+  }
+
+  SECTION("Test zeroing cu::DeviceMemory synchronously") {
+    const size_t N = 3;
+    const size_t size = N * sizeof(int);
+    cu::HostMemory src(size);
+    cu::HostMemory tgt(size);
+
+    // Populate the memory with values
+    int* const src_ptr = static_cast<int*>(src);
+    int* const tgt_ptr = static_cast<int*>(tgt);
+    for (int i = 0; i < N; i++) {
+      src_ptr[i] = i;
+      tgt_ptr[i] = 0;
+    }
+    cu::DeviceMemory mem(size);
+
+    cu::Stream stream;
+    stream.memcpyHtoDAsync(mem, src, size);
+    stream.synchronize();
+    mem.zero(size);
+    stream.memcpyDtoHAsync(tgt, mem, size);
+    stream.synchronize();
+
+    CHECK(static_cast<bool>(memcmp(src, tgt, size)));
+  }
+}
