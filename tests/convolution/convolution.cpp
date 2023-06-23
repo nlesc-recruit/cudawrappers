ff0925ebd1cd7f61991899ba03a45c1a99e852c3
mancini <mancini@astron.nl>
2023-06-23 14:16:51 +0200

    Add values tests

--- tests/convolution/convolution.cpp
+++ tests/convolution/convolution.cpp
@@ -29,26 +29,82 @@ void generateSignal(cufftComplex *signal, unsigned signalSize) {
   }
 }
 
+inline float norm(cufftComplex a) { return sqrt(a.x * a.x + a.y * a.y); }
+
+inline float absoluteDistance(cufftComplex a, cufftComplex b) {
+  return norm(cufftComplex{a.x - b.x, b.x - b.y});
+}
+
+struct ArrayComparisonStats {
+  float totalDifference;
+  unsigned exceedingCount;
+
+  float meanDifference;
+  float exceedingFraction;
+
+  static struct ArrayComparisonStats computeStats(float totalDifference,
+                                                  unsigned exceedingCount,
+                                                  unsigned totalCount) {
+    return ArrayComparisonStats{totalDifference, exceedingCount,
+                                totalDifference / float(totalCount),
+                                float(exceedingCount) / float(totalCount)};
+  }
+};
+using ArrayComparisonStats = struct ArrayComparisonStats;
+
+ArrayComparisonStats compareResults(cufftComplex *signal,
+                                    cufftComplex *expected, unsigned linearSize,
+                                    float tolerance) {
+  float totalDiff = 0.;
+  unsigned exceedingCount = 0;
+  const unsigned totalElements = linearSize * linearSize;
+  for (int i = 0; i < totalElements; i++) {
+    const float adistance = absoluteDistance(signal[i], expected[i]);
+    totalDiff += adistance;
+    if (adistance > tolerance * norm(expected[i])) exceedingCount++;
+  }
+
+  return ArrayComparisonStats::computeStats(totalDiff, exceedingCount,
+                                            totalElements);
+}
+
 int main(int argc, char *argv[]) {
   const unsigned fftSize = 1024u;
   const size_t arraySize = sizeof(cufftComplex) * fftSize * fftSize;
-
+  std::cout << "HERE WE ARE";
   cu::init();
   cu::Device device(0);
   cu::Context context(CU_CTX_SCHED_BLOCKING_SYNC, device);
   cu::DeviceMemory in_dev(arraySize), out_dev(arraySize);
-  cu::HostMemory in_host(arraySize), out_host(arraySize);
+  cu::HostMemory in_host(arraySize), out_host(arraySize),
+      out_testing(arraySize);
 
   cu::Stream my_stream;
   cufftComplex *in = in_host;
   cufftComplex *out = out_host;
-
   generateSignal(in, fftSize);
+
+
   my_stream.memcpyHtoDAsync(in_dev, in, arraySize);
   cufft::FFT<cufftComplex, cufftComplex, 2> fft{fftSize, fftSize};
   fft.setStream(my_stream);
   fft.execute(in_dev, out_dev, CUFFT_FORWARD);
   my_stream.memcpyDtoHAsync(out_host, out_dev, arraySize);
   my_stream.synchronize();
-  store_array_to_file(out, "output.dat", fftSize);
+
+
+  fft.execute(out_dev, in_dev, CUFFT_INVERSE);
+  my_stream.memcpyDtoHAsync(out_testing, in_dev, arraySize);
+  my_stream.synchronize();
+
+  ArrayComparisonStats result = compareResults(in,
+                                               out_testing,
+                                               fftSize,
+                                               1.e-6);
+
+  std::cout<< "Total difference is "<< result.totalDifference << "\n"<<
+              "Mean difference is "<< result.meanDifference << "\n"<<
+              "Total count is "<< result.exceedingCount << "\n"<<
+              "Percentage count is "<< result.exceedingFraction << "\n";
+
 }
