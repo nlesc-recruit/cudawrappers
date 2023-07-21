7032e340b9a4c3cc4e677a5c755e14ea169e2b29
Bouwe Andela <b.andela@esciencecenter.nl>
2023-07-21 09:41:25 +0200

    Add cufft library (#151)
    * Add cufft library for 1D and 2D complex-to-complex FFTs
    * Add tests
    
    Co-authored-by: Bram Veenboer <bram.veenboer@gmail.com>
    Co-authored-by: mancini <mancini@astron.nl>

---
+++ include/cudawrappers/cufft.hpp
@@ -0,0 +1,221 @@
+#if !defined CUFFT_H
+#define CUFFT_H
+
+#include <cuda_fp16.h>
+#include <cufft.h>
+#include <cufftXt.h>
+
+#include <exception>
+
+#include "cudawrappers/cu.hpp"
+
+/*
+ * Error handling helper function, copied from cuda-samples Common/helper_cuda.h
+ */
+static const char *_cudaGetErrorEnum(cufftResult error) {
+  switch (error) {
+    case CUFFT_SUCCESS:
+      return "CUFFT_SUCCESS";
+
+    case CUFFT_INVALID_PLAN:
+      return "CUFFT_INVALID_PLAN";
+
+    case CUFFT_ALLOC_FAILED:
+      return "CUFFT_ALLOC_FAILED";
+
+    case CUFFT_INVALID_TYPE:
+      return "CUFFT_INVALID_TYPE";
+
+    case CUFFT_INVALID_VALUE:
+      return "CUFFT_INVALID_VALUE";
+
+    case CUFFT_INTERNAL_ERROR:
+      return "CUFFT_INTERNAL_ERROR";
+
+    case CUFFT_EXEC_FAILED:
+      return "CUFFT_EXEC_FAILED";
+
+    case CUFFT_SETUP_FAILED:
+      return "CUFFT_SETUP_FAILED";
+
+    case CUFFT_INVALID_SIZE:
+      return "CUFFT_INVALID_SIZE";
+
+    case CUFFT_UNALIGNED_DATA:
+      return "CUFFT_UNALIGNED_DATA";
+
+    case CUFFT_INCOMPLETE_PARAMETER_LIST:
+      return "CUFFT_INCOMPLETE_PARAMETER_LIST";
+
+    case CUFFT_INVALID_DEVICE:
+      return "CUFFT_INVALID_DEVICE";
+
+    case CUFFT_PARSE_ERROR:
+      return "CUFFT_PARSE_ERROR";
+
+    case CUFFT_NO_WORKSPACE:
+      return "CUFFT_NO_WORKSPACE";
+
+    case CUFFT_NOT_IMPLEMENTED:
+      return "CUFFT_NOT_IMPLEMENTED";
+
+    case CUFFT_LICENSE_ERROR:
+      return "CUFFT_LICENSE_ERROR";
+
+    case CUFFT_NOT_SUPPORTED:
+      return "CUFFT_NOT_SUPPORTED";
+  }
+
+  return "<unknown>";
+}
+
+namespace cufft {
+
+/*
+ * Error
+ */
+class Error : public std::exception {
+ public:
+  explicit Error(cufftResult result) : result_(result) {}
+
+  const char *what() const noexcept override {
+    return _cudaGetErrorEnum(result_);
+  }
+
+  operator cufftResult() const { return result_; }
+
+ private:
+  cufftResult result_;
+};
+
+/*
+ * FFT
+ */
+class FFT {
+ public:
+  FFT() = default;
+  FFT &operator=(FFT &) = delete;
+  FFT(FFT &) = delete;
+  FFT &operator=(FFT &&other) noexcept {
+    if (&other != this) {
+      plan_ = other.plan_;
+      other.plan_ = 0;
+    }
+    return *this;
+  }
+  FFT(FFT &&other) noexcept { *this = std::move(other); }
+
+  ~FFT() { checkCuFFTCall(cufftDestroy(plan_)); }
+
+  void setStream(CUstream stream) {
+    checkCuFFTCall(cufftSetStream(plan_, stream));
+  }
+
+  void execute(cu::DeviceMemory &in, cu::DeviceMemory &out,
+               int direction = CUFFT_FORWARD) {
+    void *in_ptr = reinterpret_cast<void *>(static_cast<CUdeviceptr>(in));
+    void *out_ptr = reinterpret_cast<void *>(static_cast<CUdeviceptr>(out));
+    checkCuFFTCall(cufftXtExec(plan_, in_ptr, out_ptr, direction));
+  }
+
+  void execute(CUdeviceptr in, CUdeviceptr out, int direction) {
+    void *in_ptr = reinterpret_cast<void *>(in);
+    void *out_ptr = reinterpret_cast<void *>(out);
+    checkCuFFTCall(cufftXtExec(plan_, in_ptr, out_ptr, direction));
+  }
+
+ protected:
+  void checkCuFFTCall(cufftResult result) {
+    if (result != CUFFT_SUCCESS) {
+      throw Error(result);
+    }
+  }
+
+  cufftHandle *plan() { return &plan_; }
+
+ private:
+  cufftHandle plan_{};
+};
+
+/*
+ * FFT1D
+ */
+template <cudaDataType_t T>
+class FFT1D : public FFT {
+ public:
+  FFT1D(int nx) = delete;
+  FFT1D(int nx, int batch) = delete;
+};
+
+template <>
+FFT1D<CUDA_C_32F>::FFT1D(int nx, int batch) {
+  checkCuFFTCall(cufftCreate(plan()));
+  checkCuFFTCall(cufftPlan1d(plan(), nx, CUFFT_C2C, batch));
+}
+
+template <>
+FFT1D<CUDA_C_32F>::FFT1D(int nx) : FFT1D(nx, 1) {}
+
+template <>
+FFT1D<CUDA_C_16F>::FFT1D(int nx, int batch) {
+  checkCuFFTCall(cufftCreate(plan()));
+  const int rank = 1;
+  size_t ws = 0;
+  std::array<long long, 1> n{nx};
+  long long int idist = 1;
+  long long int odist = 1;
+  int istride = 1;
+  int ostride = 1;
+  checkCuFFTCall(cufftXtMakePlanMany(*plan(), rank, n.data(), nullptr, istride,
+                                     idist, CUDA_C_16F, nullptr, ostride, odist,
+                                     CUDA_C_16F, batch, &ws, CUDA_C_16F));
+}
+
+template <>
+FFT1D<CUDA_C_16F>::FFT1D(int nx) : FFT1D(nx, 1) {}
+
+/*
+ * FFT2D
+ */
+template <cudaDataType_t T>
+class FFT2D : public FFT {
+ public:
+  FFT2D(int nx, int ny) = delete;
+  FFT2D(int nx, int ny, int stride, int dist, int batch) = delete;
+};
+
+template <>
+FFT2D<CUDA_C_32F>::FFT2D(int nx, int ny) {
+  checkCuFFTCall(cufftCreate(plan()));
+  checkCuFFTCall(cufftPlan2d(plan(), nx, ny, CUFFT_C2C));
+}
+
+template <>
+FFT2D<CUDA_C_32F>::FFT2D(int nx, int ny, int stride, int dist, int batch) {
+  checkCuFFTCall(cufftCreate(plan()));
+  std::array<int, 2> n{nx, ny};
+  checkCuFFTCall(cufftPlanMany(plan(), 2, n.data(), n.data(), stride, dist,
+                               n.data(), stride, dist, CUFFT_C2C, batch));
+}
+
+template <>
+FFT2D<CUDA_C_16F>::FFT2D(int nx, int ny, int stride, int dist, int batch) {
+  checkCuFFTCall(cufftCreate(plan()));
+  const int rank = 2;
+  size_t ws = 0;
+  std::array<long long, 2> n{nx, ny};
+  int istride = stride;
+  int ostride = stride;
+  long long int idist = dist;
+  long long int odist = dist;
+  checkCuFFTCall(cufftXtMakePlanMany(*plan(), rank, n.data(), nullptr, istride,
+                                     idist, CUDA_C_16F, nullptr, ostride, odist,
+                                     CUDA_C_16F, batch, &ws, CUDA_C_16F));
+}
+
+template <>
+FFT2D<CUDA_C_16F>::FFT2D(int nx, int ny) : FFT2D(nx, ny, 1, nx * ny, 1) {}
+
+}  // namespace cufft
+
+#endif  // CUFFT_H
---
+++ tests/test_cufft.cpp
@@ -0,0 +1,214 @@
+#include <fstream>
+#include <iostream>
+
+#include <catch2/catch.hpp>
+#include <cudawrappers/cufft.hpp>
+
+#define FP16_EPSILON 1e-3f
+#define FP32_EPSILON 1e-6f
+
+template <typename T>
+void generateSignal(T *in, size_t size, size_t patchSize, T signal) {
+  for (size_t i = 0; i < patchSize; i++) {
+    in[i] = signal;
+  }
+}
+
+template <typename T>
+void generateSignal(T *in, size_t height, size_t width, size_t patchSize,
+                    T signal) {
+  for (size_t i = 0; i < patchSize; i++) {
+    for (size_t j = 0; j < patchSize; j++) {
+      in[(width * i) + j] = signal;
+    }
+  }
+}
+
+template <typename T>
+void scaleSignal(T *in, T *out, size_t n, float scale) {
+  for (size_t i = 0; i < n; i++) {
+    out[i].x = in[i].x / scale;
+    out[i].y = in[i].y / scale;
+  }
+}
+
+void compare(float a, float b, double epsilon = FP32_EPSILON) {
+  REQUIRE_THAT(a, Catch::Matchers::WithinAbs(b, epsilon));
+}
+
+void compare(half a, half b, double epsilon = FP16_EPSILON) {
+  compare(__half2float(a), __half2float(b), epsilon);
+}
+
+template <typename T>
+void compare(T a, T b) {
+  compare(a.x, b.x);
+  compare(a.y, b.y);
+}
+
+template <typename T>
+void compare(T *a, T *b, size_t n) {
+  for (size_t i = 0; i < n; i++) {
+    compare(a[i], b[i]);
+  }
+}
+
+TEST_CASE("Test 1D FFT", "[FFT1D]") {
+  cu::init();
+  cu::Device device(0);
+  cu::Context context(CU_CTX_SCHED_BLOCKING_SYNC, device);
+  cu::Stream stream;
+
+  const size_t size = 256;
+  const size_t patchSize = 10;
+
+  SECTION("FP32") {
+    const size_t arraySize = size * sizeof(cufftComplex);
+
+    cu::HostMemory h_in(arraySize);
+    cu::HostMemory h_out(arraySize);
+    cu::DeviceMemory d_in(arraySize);
+    cu::DeviceMemory d_out(arraySize);
+    cu::DeviceMemory d_out2(arraySize);
+
+    generateSignal(static_cast<cufftComplex *>(h_in), size, patchSize, {1, 1});
+    stream.memcpyHtoDAsync(d_in, h_in, arraySize);
+
+    cufft::FFT1D<CUDA_C_32F> fft{size};
+    fft.setStream(stream);
+
+    fft.execute(d_in, d_out, CUFFT_FORWARD);
+    fft.execute(d_out, d_out2, CUFFT_INVERSE);
+    stream.memcpyDtoHAsync(h_out, d_out2, arraySize);
+    stream.synchronize();
+
+    cuFloatComplex *in_ptr = static_cast<cuFloatComplex *>(h_in);
+    cuFloatComplex *out_ptr = static_cast<cuFloatComplex *>(h_out);
+    scaleSignal(out_ptr, out_ptr, size, float(size));
+    compare(out_ptr, in_ptr, size);
+  }
+
+  SECTION("FP16") {
+    const size_t arraySize = size * sizeof(half2);
+
+    cu::HostMemory h_in(arraySize);
+    cu::HostMemory h_out(arraySize);
+    cu::DeviceMemory d_in(arraySize);
+    cu::DeviceMemory d_out(arraySize);
+    cu::DeviceMemory d_out2(arraySize);
+
+    generateSignal(static_cast<half2 *>(h_in), 1, size, patchSize, {0.1, 0.1});
+    stream.memcpyHtoDAsync(d_in, h_in, arraySize);
+
+    cufft::FFT1D<CUDA_C_16F> fft{size};
+    fft.setStream(stream);
+
+    fft.execute(d_in, d_out, CUFFT_FORWARD);
+    fft.execute(d_out, d_out2, CUFFT_INVERSE);
+    stream.memcpyDtoHAsync(h_out, d_out2, arraySize);
+    stream.synchronize();
+
+    half2 *in_ptr = static_cast<half2 *>(h_in);
+    half2 *out_ptr = static_cast<half2 *>(h_out);
+    scaleSignal(out_ptr, out_ptr, size, float(size));
+    compare(out_ptr, in_ptr, size);
+  }
+}
+
+TEST_CASE("Test 2D FFT", "[FFT2D]") {
+  cu::init();
+  cu::Device device(0);
+  cu::Context context(CU_CTX_SCHED_BLOCKING_SYNC, device);
+  cu::Stream stream;
+
+  const size_t height = 256;
+  const size_t width = height;
+  const size_t patchSize = 10;
+
+  SECTION("FP32") {
+    const size_t arraySize = height * width * sizeof(cufftComplex);
+
+    cu::HostMemory h_in(arraySize);
+    cu::HostMemory h_out(arraySize);
+    cu::DeviceMemory d_in(arraySize);
+    cu::DeviceMemory d_out(arraySize);
+    cu::DeviceMemory d_out2(arraySize);
+
+    generateSignal(static_cast<cufftComplex *>(h_in), height, width, patchSize,
+                   {1, 1});
+    stream.memcpyHtoDAsync(d_in, h_in, arraySize);
+
+    cufft::FFT2D<CUDA_C_32F> fft{height, width};
+    fft.setStream(stream);
+
+    fft.execute(d_in, d_out, CUFFT_FORWARD);
+    fft.execute(d_out, d_out2, CUFFT_INVERSE);
+    stream.memcpyDtoHAsync(h_out, d_out2, arraySize);
+    stream.synchronize();
+
+    cuFloatComplex *in_ptr = static_cast<cuFloatComplex *>(h_in);
+    cuFloatComplex *out_ptr = static_cast<cuFloatComplex *>(h_out);
+    scaleSignal(out_ptr, out_ptr, height * width, float(height * width));
+    compare(out_ptr, in_ptr, height * width);
+  }
+
+  SECTION("FP32 batched") {
+    const size_t batch = 2;
+    const size_t arraySize = batch * height * width * sizeof(cufftComplex);
+
+    cu::HostMemory h_in(arraySize);
+    cu::HostMemory h_out(arraySize);
+    cu::DeviceMemory d_in(arraySize);
+    cu::DeviceMemory d_out(arraySize);
+    cu::DeviceMemory d_out2(arraySize);
+
+    const size_t stride = 1;
+    const size_t dist = height * width;
+
+    generateSignal(static_cast<cuFloatComplex *>(h_in), height, width,
+                   patchSize, {1, 1});
+    generateSignal(static_cast<cuFloatComplex *>(h_in) + dist, height, width,
+                   patchSize, {2, 2});
+    stream.memcpyHtoDAsync(d_in, h_in, arraySize);
+
+    cufft::FFT2D<CUDA_C_32F> fft{height, width, stride, dist, batch};
+    fft.setStream(stream);
+
+    fft.execute(d_in, d_out, CUFFT_FORWARD);
+    fft.execute(d_out, d_out2, CUFFT_INVERSE);
+    stream.memcpyDtoHAsync(h_out, d_out2, arraySize);
+    stream.synchronize();
+
+    cuFloatComplex *in_ptr = static_cast<cuFloatComplex *>(h_in);
+    cuFloatComplex *out_ptr = static_cast<cuFloatComplex *>(h_out);
+    scaleSignal(out_ptr, out_ptr, height * width, float(height * width));
+    compare(out_ptr, in_ptr, height * width);
+  }
+
+  SECTION("FP16") {
+    const size_t arraySize = height * width * sizeof(__half2);
+
+    cu::HostMemory h_in(arraySize);
+    cu::HostMemory h_out(arraySize);
+    cu::DeviceMemory d_in(arraySize);
+    cu::DeviceMemory d_out(arraySize);
+    cu::DeviceMemory d_out2(arraySize);
+
+    generateSignal(static_cast<half2 *>(h_in), height, width, patchSize,
+                   {0.1, 0.1});
+    stream.memcpyHtoDAsync(d_in, h_in, arraySize);
+
+    cufft::FFT2D<CUDA_C_16F> fft{height, width};
+    fft.setStream(stream);
+
+    fft.execute(d_in, d_out, CUFFT_FORWARD);
+    fft.execute(d_out, d_out2, CUFFT_INVERSE);
+    stream.memcpyDtoHAsync(h_out, d_out2, arraySize);
+    stream.synchronize();
+
+    half2 *in_ptr = static_cast<half2 *>(h_in);
+    half2 *out_ptr = static_cast<half2 *>(h_out);
+    scaleSignal(out_ptr, out_ptr, height * width, float(height * width));
+    compare(out_ptr, in_ptr, height * width);
+  }
+}
