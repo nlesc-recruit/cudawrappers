c9bcc0ddcc90bdab6ba93d386236cfd726cda240
Bram Veenboer <bram.veenboer@gmail.com>
2023-09-13 16:05:11 +0200

    Add cu::RegisteredMemory

--- include/cudawrappers/cu.hpp
+++ include/cudawrappers/cu.hpp
@@ -240,6 +240,18 @@ class HostMemory : public Wrapper<void *> {
   }
 };
 
+class RegisteredMemory : public HostMemory {
+ public:
+  explicit RegisteredMemory(void * ptr, size_t size, unsigned int flags = 0) : HostMemory(0) {
+    _obj = ptr;
+    checkCudaCall(cuMemHostRegister(&_obj, size, flags));
+    manager = std::shared_ptr<void *>(new (void *)(_obj), [](void **ptr) {
+      cuMemHostUnregister(*ptr);
+      delete ptr;
+    });
+  }
+};
+
 class DeviceMemory : public Wrapper<CUdeviceptr> {
  public:
   explicit DeviceMemory(size_t size) {
