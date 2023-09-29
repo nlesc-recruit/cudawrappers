0c2b7bec0783c95d3781be80e67d446d7ede4d4b
Bram Veenboer <bram.veenboer@gmail.com>
2023-09-29 12:56:13 +0200

    Use ld to embed kernel source into object file

--- cmake/helper.cmake
+++ cmake/helper.cmake
@@ -12,3 +12,19 @@ function(include_cuda_code target input_file)
     ${target} PRIVATE "${CMAKE_CURRENT_BINARY_DIR}/${output_subdir}"
   )
 endfunction(include_cuda_code)
+
+function(target_embed_source target input_file)
+  get_filename_component(NAME ${input_file} NAME_WLE)
+  add_custom_command(
+    OUTPUT ${NAME}.o
+    COMMAND ld ARGS -r -b binary -o ${CMAKE_CURRENT_BINARY_DIR}/${NAME}.o
+            ${input_file}
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+    DEPENDS ${input_file}
+  )
+  if(NOT TARGET ${NAME})
+    add_library(${NAME} STATIC ${NAME}.o)
+    set_target_properties(${NAME} PROPERTIES LINKER_LANGUAGE CXX)
+  endif()
+  target_link_libraries(${target} PRIVATE ${NAME})
+endfunction()
