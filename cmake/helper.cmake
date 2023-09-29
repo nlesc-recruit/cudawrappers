c12724e8b0a6000eb404a1d8f99bcbf6444dec91
Bram Veenboer <bram.veenboer@gmail.com>
2023-09-29 15:31:53 +0200

    Remove include_cuda_code

--- cmake/helper.cmake
+++ cmake/helper.cmake
@@ -1,18 +1,3 @@
-# Make it possible to #include cuda source code
-function(include_cuda_code target input_file)
-  # Save file containing cuda code as a C++ raw string literal
-  file(READ ${input_file} content)
-  set(delim "for_c++_include")
-  set(content "R\"${delim}(\n${content})${delim}\"")
-  set(output_file "${CMAKE_CURRENT_BINARY_DIR}/${input_file}")
-  file(WRITE ${output_file} "${content}")
-  # Add save path to the include directories
-  get_filename_component(output_subdir ${input_file} DIRECTORY)
-  target_include_directories(
-    ${target} PRIVATE "${CMAKE_CURRENT_BINARY_DIR}/${output_subdir}"
-  )
-endfunction(include_cuda_code)
-
 # Make it possible to embed a source file in a library, and link it to a target.
 # E.g. to link <kernel.cu> into target <example_program>, use
 # target_embed_source(example_program, kernel.cu). This will expose symbols
