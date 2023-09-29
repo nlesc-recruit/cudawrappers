f0a1861c1d5fde45de7401c4da9e8ce9ae0a1786
Bram Veenboer <bram.veenboer@gmail.com>
2023-09-29 15:31:42 +0200

    Add documentation

--- cmake/helper.cmake
+++ cmake/helper.cmake
@@ -13,8 +13,14 @@ function(include_cuda_code target input_file)
   )
 endfunction(include_cuda_code)
 
+# Make it possible to embed a source file in a library, and link it to a target.
+# E.g. to link <kernel.cu> into target <example_program>, use
+# target_embed_source(example_program, kernel.cu). This will expose symbols
+# _binary_kernel_cu_start and _binary_ls_kernel_cu_end.
 function(target_embed_source target input_file)
+  # Strip the path and extension from input_file
   get_filename_component(NAME ${input_file} NAME_WLE)
+  # Link the input_file into an object file
   add_custom_command(
     OUTPUT ${NAME}.o
     COMMAND ld ARGS -r -b binary -o ${CMAKE_CURRENT_BINARY_DIR}/${NAME}.o
@@ -23,8 +29,10 @@ function(target_embed_source target input_file)
     DEPENDS ${input_file}
   )
   if(NOT TARGET ${NAME})
+    # Create a proper static library for the .o file
     add_library(${NAME} STATIC ${NAME}.o)
     set_target_properties(${NAME} PROPERTIES LINKER_LANGUAGE CXX)
   endif()
+  # Link the static library to the target
   target_link_libraries(${target} PRIVATE ${NAME})
 endfunction()
