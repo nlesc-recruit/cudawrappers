e4bbb40660aee97eb1a9b34c54af192310a42f7b
Mattia Mancini <mancini@astron.nl>
2023-10-03 16:04:49 +0200

    Add cuda dependency checking

--- cmake/cudawrappers-targets.cmake
+++ cmake/cudawrappers-targets.cmake
@@ -1,26 +1,3 @@
-# You can use cudawrappers by installing it, or by including it in your project
-set(CUDAWRAPPERS_INSTALLED ${CMAKE_PROJECT_NAME} STREQUAL cudawrappers)
-
-# cudawrappers requires the CUDA Toolkit.If you include cudawrappers in your
-# project, you need to include the toolkit yourself
-set(CUDA_MIN_VERSION 10.0)
-if(${CUDAWRAPPERS_INSTALLED})
-  find_package(CUDAToolkit ${CUDA_MIN_VERSION} REQUIRED)
-else()
-  if(${CUDAToolkit_FOUND})
-    if(${CUDAToolkit_VERSION_MAJOR} LESS ${CUDA_MIN_VERSION})
-      message(FATAL_ERROR "Insufficient CUDA version: " ${CUDAToolkit_VERSION}
-                          " < " ${CUDA_MIN_VERSION}
-      )
-    endif()
-  else()
-    message(
-      FATAL_ERROR
-        "CUDAToolkit not found, use find_package(CUDAToolkit REQUIRED)."
-    )
-  endif()
-endif()
-
 # Get the include directory in the source tree
 get_filename_component(
   CUDAWRAPPERS_INCLUDE_DIR ${CMAKE_CURRENT_LIST_DIR}/../include ABSOLUTE
