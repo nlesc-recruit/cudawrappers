3b6584e23182d5c84ea815b4acfa57510ffe64cc
Hanno Spreeuw <h.spreeuw@esciencecenter.nl>
2024-03-21 12:14:02 +0100

    These are probably remnants from the past, when cudawrappers was still building libraries instead of only header files. Not useful now and hampers building a HIP only version.

--- cmake/cudawrappers-targets.cmake
+++ cmake/cudawrappers-targets.cmake
@@ -5,11 +5,11 @@ get_filename_component(
 
 # Define all the individual components that cudawrappers provides
 set(CUDAWRAPPERS_COMPONENTS cu cufft nvrtc nvtx)
-set(LINK_cu CUDA::cuda_driver)
-set(LINK_cufft CUDA::cuda_driver CUDA::cufft)
-set(LINK_nvrtc CUDA::cuda_driver CUDA::nvrtc)
-set(LINK_nvtx CUDA::nvToolsExt)
-
+# set(LINK_cu CUDA::cuda_driver)
+# set(LINK_cufft CUDA::cuda_driver CUDA::cufft)
+# set(LINK_nvrtc CUDA::cuda_driver CUDA::nvrtc)
+# set(LINK_nvtx CUDA::nvToolsExt)
+# 
 foreach(component ${CUDAWRAPPERS_COMPONENTS})
   add_library(${component} INTERFACE)
   # cudawrappers exposes targets like, cudawrappers::cu, so an alias is created
