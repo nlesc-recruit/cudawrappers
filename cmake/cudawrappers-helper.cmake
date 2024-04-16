cc357e5649c8ae7b4fbcd4071c6725c2c216b893
Bram Veenboer <veenboer@astron.nl>
2024-04-16 14:50:23 +0200

    Let target_embed_source inline local header files (#263)

--- cmake/cudawrappers-helper.cmake
+++ cmake/cudawrappers-helper.cmake
@@ -1,3 +1,23 @@
+# Copy the contents of the input file to the output file with all the local
+# includes inlined. Local includes are assumed to have ""'s, e.g. having a line
+# '#include "helper.h"` will lead to `helper.h` being inlined.
+function(inline_local_includes input_file output_file)
+  file(READ ${input_file} input_file_contents)
+  set(include_regex "#include[ \t]*\"([^\"]+)\"")
+  string(REGEX MATCHALL ${include_regex} includes ${input_file_contents})
+  string(REGEX REPLACE ${include_regex} "" input_file_contents
+                       "${input_file_contents}"
+  )
+  foreach(include ${includes})
+    string(REGEX REPLACE ${include_regex} "\\1" include ${include})
+    file(GLOB_RECURSE INCLUDE_PATH "${CMAKE_SOURCE_DIR}/*/${include}")
+    file(READ ${INCLUDE_PATH} include_contents)
+    string(APPEND processed_file_contents "${include_contents}\n\n")
+  endforeach()
+  string(APPEND processed_file_contents "${input_file_contents}\n")
+  file(WRITE ${output_file} "${processed_file_contents}")
+endfunction()
+
 # Make it possible to embed a source file in a library, and link it to a target.
 # E.g. to link <kernel.cu> into target <example_program>, use
 # target_embed_source(example_program, kernel.cu). This will expose symbols
@@ -6,13 +26,18 @@ function(target_embed_source target input_file)
   include(CMakeDetermineSystem)
   # Strip the path and extension from input_file
   get_filename_component(NAME ${input_file} NAME_WLE)
+  # Make a copy of the input file in the binary dir with inlined header files
+  set(input_file_inlined "${CMAKE_BINARY_DIR}/${input_file}")
+  inline_local_includes(
+    "${CMAKE_CURRENT_SOURCE_DIR}/${input_file}" ${input_file_inlined}
+  )
   # Link the input_file into an object file
   add_custom_command(
     OUTPUT ${NAME}.o
     COMMAND ld ARGS -r -b binary -A ${CMAKE_SYSTEM_PROCESSOR} -o
             ${CMAKE_CURRENT_BINARY_DIR}/${NAME}.o ${input_file}
-    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-    DEPENDS ${input_file}
+    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+    DEPENDS ${input_file} ${input_file_inlined}
     COMMENT "Creating object file for ${input_file}"
   )
   if(NOT TARGET ${NAME})
