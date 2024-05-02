5e29ebbbb7d847d53c5b9b59f0fc75536eadf007
Bram Veenboer <bram.veenboer@gmail.com>
2024-05-02 16:06:47 +0200

    Fix string replace

--- cmake/cudawrappers-helper.cmake
+++ cmake/cudawrappers-helper.cmake
@@ -29,8 +29,8 @@ function(target_embed_source target input_file)
   # Get absolute path for input file
   get_filename_component(input_file_absolute ${input_file} ABSOLUTE)
   # Make a copy of the input file in the binary dir with inlined header files
-  string(REPLACE ${CMAKE_CURRENT_SOURCE_DIR} ${CMAKE_BINARY_DIR}
-                 input_file_inlined ${input_file_absolute}
+  string(REPLACE "${CMAKE_SOURCE_DIR}" "${CMAKE_BINARY_DIR}" input_file_inlined
+                 ${input_file_absolute}
   )
   inline_local_includes(${input_file_absolute} ${input_file_inlined})
   # Link the input_file into an object file
