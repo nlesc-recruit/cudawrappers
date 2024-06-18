be96b76d6fb1d86da833c126834fdc4e335966c9
Leon Oostrum <l.oostrum@esciencecenter.nl>
2024-06-18 14:36:49 +0200

    Add HIP version of occupancyMaxActiveBlocksPerMultiprocessor

--- include/cudawrappers/cu.hpp
+++ include/cudawrappers/cu.hpp
@@ -209,6 +209,15 @@ class Context : public Wrapper<hipCtx_t> {
     return config;
   }
 
+ int occupancyMaxActiveBlocksPerMultiprocessor(int blockSize,
+                                               size_t dynamicSMemSize) {
+   int numBlocks;
+   checkCudaCall(hipModuleOccupancyMaxActiveBlocksPerMultiprocessor(
+       &numBlocks, _obj, blockSize, dynamicSMemSize));
+   return numBlocks;
+ }
+
+
   static void setCacheConfig(hipFuncCache_t config) {
     checkCudaCall(hipCtxSetCacheConfig(config));
   }
