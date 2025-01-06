ad13d8fb690840e6f6407dce63c6c99243b00da5
Bram Veenboer <veenboer@astron.nl>
2025-01-06 16:40:53 +0100

    Update nvml interface (#308)
    * Remove unused context agument from nvml::Device
    * Add missing checkNvmlCall
    * Make nvml::Device functions const
    * Add pass_filenames option to workaround cppcheck error

--- tests/test_nvml.cpp
+++ tests/test_nvml.cpp
@@ -9,12 +9,12 @@ TEST_CASE("Test nvml::Context", "[context]") { nvml::Context context; }
 
 TEST_CASE("Test nvml::Device with device number", "[device]") {
   nvml::Context context;
-  nvml::Device device(context, 0);
+  nvml::Device device(0);
 }
 
 TEST_CASE("Test nvml::Device::getClock", "[device]") {
   nvml::Context context;
-  nvml::Device device(context, 0);
+  nvml::Device device(0);
   const unsigned int clockMHz =
       device.getClock(NVML_CLOCK_GRAPHICS, NVML_CLOCK_ID_CURRENT);
   REQUIRE(clockMHz > 0);
@@ -22,7 +22,7 @@ TEST_CASE("Test nvml::Device::getClock", "[device]") {
 
 TEST_CASE("Test nvml::Device::getPower", "[device]") {
   nvml::Context context;
-  nvml::Device device(context, 0);
+  nvml::Device device(0);
   const unsigned int power = device.getPower();
   REQUIRE(power > 0);
 }
@@ -31,5 +31,5 @@ TEST_CASE("Test nvml::Device with device", "[device]") {
   cu::init();
   cu::Device cu_device(0);
   nvml::Context nvml_context;
-  nvml::Device nvml_device(nvml_context, cu_device);
+  nvml::Device nvml_device(cu_device);
 }
