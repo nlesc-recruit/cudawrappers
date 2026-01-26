abc8ef744fa11ffd50956d2ae906f2c3510d8b69
Bram Veenboer <veenboer@astron.nl>
2026-01-26 14:52:27 +0100

    Fix install interface for targets (#371)
    * Remove conditional branch
    * Remove unneeded CUDAWRAPPERS_INSTALLED
    * Remove redundant error message for finding CUDAToolkit

--- cmake/cudawrappers-dependencies.cmake
+++ cmake/cudawrappers-dependencies.cmake
@@ -82,20 +82,10 @@ else()
   # cudawrappers requires the CUDA Toolkit.If you include cudawrappers in your
   # project, you need to include the toolkit yourself
   set(CUDA_MIN_VERSION 10.0)
-  if(${CUDAWRAPPERS_INSTALLED})
-    find_package(CUDAToolkit ${CUDA_MIN_VERSION} REQUIRED)
-  else()
-    if(${CUDAToolkit_FOUND})
-      if(${CUDAToolkit_VERSION_MAJOR} LESS ${CUDA_MIN_VERSION})
-        message(FATAL_ERROR "Insufficient CUDA version: "
-                            ${CUDAToolkit_VERSION} " < " ${CUDA_MIN_VERSION}
-        )
-      endif()
-    else()
-      message(
-        FATAL_ERROR
-          "CUDAToolkit not found, use find_package(CUDAToolkit REQUIRED)."
-      )
-    endif()
+  find_package(CUDAToolkit ${CUDA_MIN_VERSION} REQUIRED)
+  if(${CUDAToolkit_VERSION_MAJOR} LESS ${CUDA_MIN_VERSION})
+    message(FATAL_ERROR "Insufficient CUDA version: " ${CUDAToolkit_VERSION}
+                        " < " ${CUDA_MIN_VERSION}
+    )
   endif()
 endif()
