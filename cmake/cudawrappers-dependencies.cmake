a1b4a6c89af4d9131880770a9d02b4fbc4a8bb79
Bram Veenboer <bram.veenboer@gmail.com>
2026-01-26 13:40:57 +0100

    Remvoe unneeded CUDAWRAPPERS_INSTALLED

--- cmake/cudawrappers-dependencies.cmake
+++ cmake/cudawrappers-dependencies.cmake
@@ -82,20 +82,17 @@ else()
   # cudawrappers requires the CUDA Toolkit.If you include cudawrappers in your
   # project, you need to include the toolkit yourself
   set(CUDA_MIN_VERSION 10.0)
-  if(${CUDAWRAPPERS_INSTALLED})
-    find_package(CUDAToolkit ${CUDA_MIN_VERSION} REQUIRED)
-  else()
-    if(${CUDAToolkit_FOUND})
-      if(${CUDAToolkit_VERSION_MAJOR} LESS ${CUDA_MIN_VERSION})
-        message(FATAL_ERROR "Insufficient CUDA version: "
-                            ${CUDAToolkit_VERSION} " < " ${CUDA_MIN_VERSION}
-        )
-      endif()
-    else()
-      message(
-        FATAL_ERROR
-          "CUDAToolkit not found, use find_package(CUDAToolkit REQUIRED)."
+  find_package(CUDAToolkit ${CUDA_MIN_VERSION} REQUIRED)
+  if(${CUDAToolkit_FOUND})
+    if(${CUDAToolkit_VERSION_MAJOR} LESS ${CUDA_MIN_VERSION})
+      message(FATAL_ERROR "Insufficient CUDA version: " ${CUDAToolkit_VERSION}
+                          " < " ${CUDA_MIN_VERSION}
       )
     endif()
+  else()
+    message(
+      FATAL_ERROR
+        "CUDAToolkit not found, use find_package(CUDAToolkit REQUIRED)."
+    )
   endif()
 endif()
