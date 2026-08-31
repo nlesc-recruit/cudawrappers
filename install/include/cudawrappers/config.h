f76c9afb3449aab3b53dd9e7ecebcc125b2fa786
Bram Veenboer <bram.veenboer@gmail.com>
2026-08-31 13:36:50 +0200

    Remove binary, debug, and temporary files
    - Deleted build artifacts (debug/, install/, tests/tests/, build directories)
    - Deleted temporary files (compile-errors.txt, hip-build-fixes.txt, tmp_*.txt)
    - Deleted debug tools (hip-query.cpp, hsa-query.c, graph.dot)
    - Deleted codacy.instructions.md

--- install/include/cudawrappers/config.h
+++
@@ -1,8 +0,0 @@
-#ifndef CUDAWRAPPERS_CONFIG_H
-#define CUDAWRAPPERS_CONFIG_H
-#define CUDA_INCLUDE_DIRS                                    \
-  "/var/software/spack/2025.10.1/opt/spack/linux-x86_64_v3/" \
-  "cuda-13.0.2-6mlkhuiide5f6t6kvwspkcjlw4fvxcym/targets/x86_64-linux/include"
-#define HIP_INCLUDE_DIRS ""
-/* #undef CUDAWRAPPERS_USE_NVTX3 */
-#endif  // CUDAWRAPPERS_CONFIG_H
